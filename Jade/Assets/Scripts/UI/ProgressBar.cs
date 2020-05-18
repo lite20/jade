10bc08d70a20a6ebfd417cae03eccdb96e889919
lite20 <hamster.team1@gmail.com>
2020-05-18 07:13:03 -0600

    init

---
+++ Jade/Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    public float percent = 1.0f;
+
+    private RectTransform barRect;
+
+    private float width;
+
+    public void Start()
+    {
+        width = GetComponent<RectTransform>().rect.width;
+        barRect = transform.GetChild(0).GetComponent<RectTransform>();
+        SetPercentage(percent);
+    }
+
+    public void SetPercentage(float percent)
+    {
+        Vector2 size = barRect.sizeDelta;
+        size.x = width * percent;
+        barRect.sizeDelta = size;
+    }
+}
